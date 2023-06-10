0ee8e9e8854b80a4c6f68cf67482ca4a01338a30
aayush105 <sthaaayush105@gmail.com>
2023-06-10 15:06:32 +0545

    complete RestFul API for Articles

--- app.js
+++ app.js
@@ -19,8 +19,12 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
+/////////////////////// Request Targeting all Articles///////////////////////
 
-app.get("/articles", async function(req, res) {
+// chained route 
+app.route("/articles")
+
+  .get(async function(req, res) {
     try {
       const foundArticles = await Article.find();
       console.log(foundArticles);
@@ -29,8 +33,84 @@ app.get("/articles", async function(req, res) {
       console.error(err);
       res.status(500).send("Internal Server Error"); // Handle the error and send an appropriate response
     }
+  })
+
+  .post(async function(req, res){
+    const newArticle = new Article({
+      title : req.body.title,
+      content : req.body.content
+    });
+    newArticle.save(function(err){
+      if (!err){
+        res.send("Successfully added a new article.");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .delete(async function(req, res){
+    try{
+      await Article.deleteMany();
+      res.send("Successfully deleted all the articles.");
+      } catch(err){
+      res.send(err);
+    }
   });
 
+
+/////////////////////// Request Targeting a specific Articles///////////////////////
+
+  app.route("/articles/:articleTitle")
+  .get(async function(req,res){
+    try{
+      const foundArticle = await Article.findOne(
+        { title: req.params.articleTitle }
+      )
+      if(foundArticle){
+        res.send(foundArticle);
+      } else {
+        res.send("NO articles matching that title was found.");
+      }
+    } catch(err){
+      res.send(err);
+    }
+    })
+    .put(async function(req, res){
+      try{
+        await Article.updateOne(
+          { title: req.params.articleTitle }, // condition
+          { title : req.body.title, content : req.body.content} //updated value
+        );
+        res.send("Successfully updated a article.");
+      } catch(err){
+        res.send(err);
+      }
+    })
+
+  // only updated the filed where user wants to update
+    .patch(async function(req, res){
+      try{
+        await Article.updateOne(
+          { title: req.params.articleTitle }, // condition
+          { $set: req.body } //updated value
+        );
+        res.send("Successfully updated a article.");
+      }catch(err){
+        res.send(err);
+      }
+    })
+
+    .delete(async function(req, res){
+      try{
+        await Article.deleteOne(
+          { title: req.params.articleTitle }
+        )
+        res.send("Successfully deleted a article.")
+      } catch(err){
+        res.send(err);
+      }
+    });
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
